f6256788304e1d2612fd32c5b418b4007b977e6d
curtisqt30 <143355730+curtisqt30@users.noreply.github.com>
2024-11-14 16:17:45 -0800

    Update server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+import sockets
+
+def start_server():
+    
+    PORT_NUMBER = 5000
+    
+    # Socket default values, AF_NET = IPv4 and SOCK_STREAM = TCP
+    serverSock = socket.socket(socket.AF_NET, socket.SOCK_STREAM)
+    serverSock.bind(('0.0.0.0', PORT_NUMBER))
+    
+    serverSock.listen(5);
+    
+    #serverSock.accept()
+    
+    while True:
+        cliSock, cliInfo = serverSock.accept()
+        
+        cliSock.close()
+    break
+        
