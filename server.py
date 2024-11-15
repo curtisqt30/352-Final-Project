9ffd2ad1114166bc84f43193895eb936a0eb1cae
curtisqt30 <143355730+curtisqt30@users.noreply.github.com>
2024-11-15 14:05:14 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,30 @@
 import socket
+import threading
 
+PORT_NUMBER = 5000
+
+# Handles communication with a single client
+def handle_client(cliSock, cliInfo):
+    print(f"Connection established with {cliInfo}")
+
+    try
+        while True:
+            # receive data or message
+            data = cliSock.recv(1024)
+            if not data:
+                break
+            print(f"Received from {cliInfo}: {data.decode()}")
+            # add more logic here?
+
+    except Exception as e:
+        print(f"An error occured with client {cliInfo}: {e}")
+    finally:
+        print(f"Closing connection with {cliInfo}")
+        cliSock.close()
+
+# Starts server and listens for incoming connections
 def start_server():
     print("Server started")
-    PORT_NUMBER = 5000
     
     # Socket default values, AF_NET = IPv4 and SOCK_STREAM = TCP
     serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,19 +33,22 @@ def start_server():
     serverSock.bind(('0.0.0.0', PORT_NUMBER))
     
     # Listen for incoming connections max 2
-    serverSock.listen(2);
+    serverSock.listen(2)
+    print(f"Server listening on {PORT_NUMBER}")
     
-    #serverSock.accept()
+    try:
+        while True:
+            # Accept incoming connection
+            cliSock, cliInfo = serverSock.accept()
+            # Create a new thread to handle client
+            client_thread = threading.thread(target=handle_client, args=(cliSock, cliInfo))
+            client_thread.start()
+    except KeyboardInterrupt:
+        print("\nServer shutting down.")    
     
-    while True:
-        # Accept incoming connection
-        cliSock, cliInfo = serverSock.accept()
-        print("Connection established with ", cliInfo)
-        
-        # File sharing logic
-       
+    finally:
         # Close socket afterwards 
-        cliSock.close()
+        serverSock.close()
 
 if __name__ == "__main__":
     start_server()
